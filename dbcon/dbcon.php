3eb9749da1f60ca1770b1474555a9d6f893ed048
amaPerera <maiperera93@gmail.com>
2016-11-19 15:32:28 +0530

    feedback done

---
+++ dbcon/dbcon.php
@@ -0,0 +1,8 @@
+<?php 
+
+$conn=mysqli_connect("localhost","root","","programming_project");
+if(!$conn){
+    echo "connection failed".mysqli_connect_error();
+    }
+
+?>
--- feedback.php
+++ feedback.php
@@ -1,139 +1,152 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Feed-Back</title>
-
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/menu/simple_menu.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/condolence_style.css">
-    <script type="text/javascript">
-    window.onload = function() {
-        var overlay = document.getElementById("overlay");
-        var popup = document.getElementById("popup");
-        overlay.style.display = "block";
-        popup.style.display = "block";
-    
-    document.getElementById("CloseBtn").onclick = function(){
-            var overlay = document.getElementById("overlay");
-            var popup = document.getElementById("popup");
-            overlay.style.display = "none";
-            popup.style.display = "none";      
-        }
-    };
-    </script>
+<title>RECEIPES Bootstarp responsive Website Template| contact :: w3layouts</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--webfont-->
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lobster+Two:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<!--Animation-->
+<script src="js/wow.min.js"></script>
+<link href="css/animate.css" rel='stylesheet' type='text/css' />
+<script>
+	new WOW().init();
+</script>
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+				});
+			});
+		</script>
+<script src="js/simpleCart.min.js"> </script>	
 </head>
 <body>
+    <!-- header-section-starts -->
+	<div class="header">
+		<div class="container">
+			<div class="top-header">
+				<div class="logo">
+					<a href="index.html"><img src="images/logo.png" class="img-responsive" alt="" /></a>
+				</div>
+				<div class="queries">
+					<p>Questions? Call us!<span>+94112 814 194 </span><label>(11AM to 11PM)</label></p>
+				</div>
+				<div class="header-right">
+						<div class="cart box_1">
+							<a href="checkout.html">
+								<h3> <span class="simpleCart_total"> Rs:0.00 </span> (<span id="simpleCart_quantity" class="simpleCart_quantity"> 0 </span> items)<img src="images/bag.png" alt=""></h3>
+							</a>	
+							<p><a href="javascript:;" class="simpleCart_empty">Empty card</a></p>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+			<div class="menu-bar">
+			<div class="container">
+				<div class="top-menu">
+					<ul>
+						<li><a href="index.html">Home</a></li>|
+						<li><a href="popular-restaurents.html">Popular Restaurants</a></li>|
+						<li><a href="order.html">Order</a></li>|
+						<li class="active"><a href="#contact" class="scroll">contact</a></li>
+						<div class="clearfix"></div>
+					</ul>
+				</div>
+				<div class="login-section">
+					<ul>
+						<li><a href="login.html">Login</a>  </li> |
+						<li><a href="register.html">Register</a> </li> |
+						<li><a href="#">Help</a></li>
+						<div class="clearfix"></div>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- header-section-ends -->
+	<div class="contact-section-page">
+		<div class="contact-head">
+		    <div class="container">
+				<h3>Feedback</h3>
+				<p>Home/Feedback</p>
+			</div>
+		</div>
+		<div class="contact_top">
+			 		<div class="container">
+			 			<div class="col-md-6 contact_left wow fadeInRight" data-wow-delay="0.4s">
+			 				<h4>Feedback Form</h4>
+			 				<p>We would love to hear your thoughts, concerns with anything,so we can improve!<br><br> Please take a minute to let us know what you think about us.</p>
+							  <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+								 <div class="form_details">
+					                 <input type="text" class="text" value="Your Name" id="yourname" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Name';}">
 
-<?php include 'temp/header.php';  ?>
+									 <textarea value="Feedback" id="fdback" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Feedback';}">Feedback</textarea>
 
-<div class = "fbdiv1" align="center">
-    <div class="fbdiv2">
-    <?php
-        require "dbcon/dbcon.php";
+									 <div class="clearfix"> </div>
+									 <div class="sub-button wow swing animated" data-wow-delay= "0.4s">
+									 	<input name="submit" type="submit" value="Send message">
+									 </div>
+						          </div>
+						       </form>
+						       <div class="col-md-6 company-right wow fadeInLeft" data-wow-delay="0.4s">
+					        	     	
+									<div class="follow-us">
+										<h3>follow us on</h3>
+										<a href="#"><i class="facebook"></i></a>
+										<a href="#"><i class="twitter"></i></a>
+										<a href="#"><i class="google-pluse"></i></a>
+									</div>
+			
+							
+							</div>
+					        </div>
 
-        $error=FALSE;
-            $yourname = $fdback = $status = ""; 
-            
-            if (isset($_POST['submit'])) {
-                
-                if(empty($_POST['yourname'])){ 
-                    $error = TRUE;
-                }else{
-                    $yourname = $_POST['yourname'];
-                }
-                
-                if(empty($_POST['fdback'])){ 
-                    $error = TRUE;
-                }else{
-                    $fdback = $_POST['fdback'];
-                }
-                
-              if(empty($_POST['status'])){ 
-                    $error = TRUE;
-                }else{
-                    $status = $_POST['status'];
-                }
-                
-                if ($error==FALSE){
-                $sql = "INSERT INTO `feedback`(`yourname`, `fdback`, `status`) VALUES ('$_POST[yourname]','$_POST[fdback]','$_POST[status]')";
+						</div>
+					</div>
 
-                if(mysqli_query($conn,$sql)){
-                    echo '<div id="overlay"></div>';
-                    echo '<div id="popup" align = "center">';
-                    echo '<div id="popcon">';
-                    echo '<div id="pophead">';
-                    echo 'Feedback submission';
-                    echo '</div>';
-                    echo '<div id="popbody">';
-                    echo 'Thank you for submiting your feedback';
-                    echo '</div>';
-                    echo '<div id="popfooter">';
-                    echo '<a id="CloseBtn" href="feedback.php">Ok</a>';
-                    echo '</div>';
-                    echo '</div>';
-                    echo '</div>';
-                    die();
-                } else{
-                    echo "<script type='text/javascript'>alert('Not successfully datatranfer!')</script>";}
-                }
-            }
-        ?>
-        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-            <div id="feedback">
-                <form id="fb5" action="feedback.php" method="post">
-                    <table id="fb6">
-                        <tr>
-                            <td colspan="2">
-                                <h1 id="headertable" align="center" style = "font-family: myfont1; color: #a86205;"><b>FEED-BACK</b></h1>
-                                <hr>
-                            </td>
-                        
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p id="headdis" align="center">
-									We would love to hear your,thoughts, concerns, problems with anything so we can improve!<br> 
-									Please take a minute to let us know what you think about us.
-								</p>
-                            </td>
-                        
-                        </tr>
-                         <tr>
-                            
-                                <td><label for="yourname"></label></td>
-                                
-                                <td><input type="text" name="yourname" id="yourname" placeholder="Your Name" required></td>
-                        
-                        </tr>
-                        <tr>
-                            
-                               <td><label for="fdback"></label></td>
-                        
-                             <td> <textarea name="fdback" id="fdback" rows="10" cols="40" placeholder="Enter your Feed-Back" required></textarea></td>
-                        
-                        </tr>
-                       
-                        <tr>
-                            
-                             <td><label for="status">Our Service : </label></span></td>
-                                
-                             <td>   <label for="fdback">Satisfied </label><input type="radio" name="status" value="Satisfied" required>
-                                    <label for="fdback">Unsatisfied </label><input type="radio" name="status" value="Unsatisfied"><br/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <input type="submit" value="Submit" name="submit" <?php if ($error==FALSE){}?>> 
-                                <input type="reset" value="Cancle" name="cancle">
-                            </td>
-                        
-                        </tr>
-                    </table>
-                </form>
-            </div>
-        </form>
-    </div>
-</div>
+	</div>
+	<!-- footer-section-starts -->
+	<div class="footer">
+		<div class="container">
+			<p class="wow fadeInLeft" data-wow-delay="0.4s">New Gihans Family restaurent</p>
+		</div>
+	</div>
+	<!-- footer-section-ends -->
+	  <script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 
-    <!-- include footer -->
-    <?php include 'temp/footer.php';  ?>
-    </body>
+</body>
 </html>
